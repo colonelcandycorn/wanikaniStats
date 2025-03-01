6d0ed75bbdc23fc8afc7183279af303ada97426e
Sarah Dylan <SarahmDylan@gmail.com>
2025-03-01 15:48:22 -0800

    Get User and Reset working
    I also created a generic test function to just print
    out API responses

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use chrono::{DateTime, Local, Utc};
+use dotenvy;
+use reqwest;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use wanikani_stats::data_processing::api_client;
 use wanikani_stats::data_processing::wanikani_data;
-use reqwest;
-use std::collections::HashMap;
-use dotenvy;
-use serde::{ Deserialize, Serialize };
-use chrono::{ DateTime, Local, Utc };
-
+use std::fmt;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct User {
     level: i32,
     username: String,
-    started_at: DateTime<Local>
+    started_at: DateTime<Local>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -22,44 +23,68 @@ struct PageData {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-struct PaginatedData<T> {
-    pages: PageData,
-    data: T
+struct Reset {
+    created_at: DateTime<Local>,
+    confirmed_at: DateTime<Local>,
+    original_level: i32,
+    target_level: i32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct VecData<T> {
+    pages: Option<PageData>,
+    data: Vec<Response<T>>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Response<T> {
-    pages: Option<PageData>,
-    data: T
+    data: T,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     dotenvy::dotenv()?;
 
-
     let wk_token = std::env::var("WK_TOKEN").expect("WK_TOKEN must be set");
-    let test_url = "https://api.wanikani.com/v2/user";
-    
-    let response = reqwest::Client::new().get(test_url).header("Authorization", format!("Bearer {}", wk_token)).send().await?;
+    let test_user_url = "https://api.wanikani.com/v2/user";
+    let resets_url = "https://api.wanikani.com/v2/resets";
 
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            match response.json::<Response<User>>().await {
+    let client = reqwest::Client::new();
+
+    type ResetStuff = VecData<Reset>;
+
+    test_api::<Response<User>>(test_user_url, &wk_token, &client).await.unwrap();
+    test_api::<ResetStuff>(&resets_url, &wk_token, &client).await.unwrap();
+
+    
+
+    Ok(())
+}
+
+async fn test_api<T>(url: &str, token: &str, client: &reqwest::Client) -> Result<(), Box<dyn std::error::Error>>
+where
+    T: DeserializeOwned + fmt::Debug,
+{
+    let response = client
+        .get(url)
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?;
+
+        match response.status() {
+            reqwest::StatusCode::OK => match response.json::<T>().await {
                 Ok(parsed) => {
                     println!("Parsed User: {:?}", parsed);
-                },
-                Err(e) => println!("Error processing user {}", e)
+                }
+                Err(e) => println!("Error processing user {}", e),
+            },
+            reqwest::StatusCode::UNAUTHORIZED => {
+                println!("Invalid Token")
+            }
+            other => {
+                panic!("{:?}", other)
             }
         }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Invalid Token")
-        }
-        other => {
-            panic!("{:?}", other)
-        }
-    }
 
 
     Ok(())
