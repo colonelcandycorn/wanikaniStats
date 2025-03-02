a39a99b266fb9e896f1f38b3a6edf3fb5d578f7b
Sarah Dylan <SarahmDylan@gmail.com>
2025-03-02 11:50:49 -0800

    Refactor into module api_client

--- src/data_processing/api_client.rs
+++ src/data_processing/api_client.rs
@@ -1,3 +1,161 @@
-pub fn temp() -> bool {
-    false
+use chrono::{DateTime, Local};
+use reqwest;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::marker::PhantomData;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct User {
+    level: i32,
+    username: String,
+    started_at: DateTime<Local>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PageData {
+    per_page: i32,
+    next_page: Option<String>,
+    last_page: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ReviewStatistic {
+    created_at: DateTime<Local>,
+    meaning_correct: i32,
+    meaning_current_streak: i32,
+    meaning_incorrect: i32,
+    meaning_max_streak: i32,
+    percentage_correct: i32,
+    reading_correct: i32,
+    reading_current_streak: i32,
+    reading_incorrect: i32,
+    reading_max_streak: i32,
+    subject_id: i32,
+    subject_type: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Meanings {
+    meaning: String,
+    primary: bool,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Subject {
+    characters: String,
+    level: i32,
+    spaced_repetition_system_id: i32,
+    meanings: Vec<Meanings>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Assignment {
+    created_at: Option<DateTime<Local>>,
+    passed_at: Option<DateTime<Local>>,
+    srs_stage: i32,
+    subject_id: i32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Reset {
+    created_at: DateTime<Local>,
+    confirmed_at: DateTime<Local>,
+    original_level: i32,
+    target_level: i32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PagedData<T> {
+    pages: Option<PageData>,
+    total_count: i32,
+    data: Vec<Response<T>>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Response<T> {
+    data: T,
+}
+
+pub struct ReqwestResponse<T> {
+    raw_response: reqwest::Response,
+    resource_type: PhantomData<T>,
+}
+
+#[derive(Debug)]
+pub struct ApiClient<'a> {
+    token: String,
+    client: &'a reqwest::Client,
+}
+
+impl<'a> ApiClient<'a> {
+    pub fn new(token: String, client: &'a reqwest::Client) -> Self {
+        ApiClient { token, client }
+    }
+
+    pub async fn get_response<T>(&self, url: &str) -> Result<ReqwestResponse<T>, reqwest::Error>
+    where
+        T: DeserializeOwned,
+    {
+        let response = self
+            .client
+            .get(url)
+            .header("Authorization", format!("Bearer {}", self.token))
+            .send()
+            .await?;
+
+        Ok(ReqwestResponse::<T> {
+            raw_response: response.error_for_status()?,
+            resource_type: PhantomData,
+        })
+    }
+
+    pub async fn raw_response_to_data<T>(
+        &self,
+        raw_response: ReqwestResponse<T>,
+    ) -> Result<T, serde_json::Error>
+    where
+        T: DeserializeOwned,
+    {
+        let parsed = raw_response
+            .raw_response
+            .json::<T>()
+            .await
+            .map_err(serde::de::Error::custom)?;
+
+        Ok(parsed)
+    }
+}
+
+#[allow(unused)]
+async fn test_api<T>(
+    url: &str,
+    token: &str,
+    client: &reqwest::Client,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    T: DeserializeOwned + fmt::Debug,
+{
+    let response = client
+        .get(url)
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?;
+
+    match response.status() {
+        reqwest::StatusCode::OK => match response.json::<T>().await {
+            Ok(parsed) => {
+                println!("Parsed User: {:?}", parsed);
+            }
+            Err(e) => println!("Error processing user {}", e),
+        },
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Invalid Token")
+        }
+        other => {
+            panic!("{:?}", other)
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,5 @@
-use chrono::{DateTime, Local, Utc};
-use dotenvy;
-use reqwest;
-use serde::de::DeserializeOwned;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use wanikani_stats::data_processing::api_client;
-use wanikani_stats::data_processing::wanikani_data;
-use std::fmt;
-
-#[derive(Deserialize, Serialize, Debug)]
-struct User {
-    level: i32,
-    username: String,
-    started_at: DateTime<Local>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct PageData {
-    per_page: i32,
-    next_page: Option<String>,
-    last_page: Option<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct ReviewStatistic {
-    created_at: DateTime<Local>,
-    meaning_correct: i32,
-    meaning_current_streak: i32,
-    meaning_incorrect: i32,
-    meaning_max_streak: i32,
-    percentage_correct: i32,
-    reading_correct: i32,
-    reading_current_streak: i32,
-    reading_incorrect: i32,
-    reading_max_streak: i32,
-    subject_id: i32,
-    subject_type: String
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Meanings {
-    meaning: String,
-    primary: bool
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Subject {
-    characters: String,
-    level: i32,
-    spaced_repetition_system_id: i32,
-    meanings: Vec<Meanings>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Assignment {
-    created_at: Option<DateTime<Local>>,
-    passed_at: Option<DateTime<Local>>,
-    srs_stage: i32,
-    subject_id: i32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Reset {
-    created_at: DateTime<Local>,
-    confirmed_at: DateTime<Local>,
-    original_level: i32,
-    target_level: i32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct PagedData<T> {
-    pages: Option<PageData>,
-    total_count: i32,
-    data: Vec<Response<T>>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Response<T> {
-    data: T,
-}
+use wanikani_stats::data_processing::api_client::ApiClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -86,50 +7,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let wk_token = std::env::var("WK_TOKEN").expect("WK_TOKEN must be set");
     let test_user_url = "https://api.wanikani.com/v2/user";
-    let resets_url = "https://api.wanikani.com/v2/resets";
-    let review_stats_url = "https://api.wanikani.com/v2/review_statistics";
-    let subject_url = "https://api.wanikani.com/v2/subjects/440";
-    let assignments_url = "https://api.wanikani.com/v2/assignments";
+    // let resets_url = "https://api.wanikani.com/v2/resets";
+    // let review_stats_url = "https://api.wanikani.com/v2/review_statistics";
+    // let subject_url = "https://api.wanikani.com/v2/subjects/440";
+    // let assignments_url = "https://api.wanikani.com/v2/assignments";
 
     let client = reqwest::Client::new();
 
-    type ResetStuff = PagedData<Reset>;
-    type ReviewStatisticStuff = PagedData<ReviewStatistic>;
+    let api_client = ApiClient::new(wk_token, &client);
 
-    test_api::<Response<User>>(test_user_url, &wk_token, &client).await.unwrap();
-    test_api::<ResetStuff>(&resets_url, &wk_token, &client).await.unwrap();
-    // test_api::<ReviewStatisticStuff>(&review_stats_url, &wk_token, &client).await.unwrap();
-    test_api::<Response<Subject>>(&subject_url, &wk_token, &client).await.unwrap();
-    test_api::<PagedData<Assignment>>(&assignments_url, &wk_token, &client).await.unwrap();
+    let raw = api_client
+        .get_response::<api_client::Response<api_client::User>>(test_user_url)
+        .await
+        .unwrap();
 
-    Ok(())
-}
+    let processed = api_client.raw_response_to_data(raw).await.unwrap();
 
-async fn test_api<T>(url: &str, token: &str, client: &reqwest::Client) -> Result<(), Box<dyn std::error::Error>>
-where
-    T: DeserializeOwned + fmt::Debug,
-{
-    let response = client
-        .get(url)
-        .header("Authorization", format!("Bearer {}", token))
-        .send()
-        .await?;
-
-        match response.status() {
-            reqwest::StatusCode::OK => match response.json::<T>().await {
-                Ok(parsed) => {
-                    println!("Parsed User: {:?}", parsed);
-                }
-                Err(e) => println!("Error processing user {}", e),
-            },
-            reqwest::StatusCode::UNAUTHORIZED => {
-                println!("Invalid Token")
-            }
-            other => {
-                panic!("{:?}", other)
-            }
-        }
-
+    println!("{:?}", processed);
 
     Ok(())
 }
