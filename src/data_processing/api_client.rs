045e7dfa64832a0919e4b167b62e5c4f2820d512
Sarah Dylan <SarahmDylan@gmail.com>
2025-03-02 22:02:33 -0800

    Cargo fmt

--- src/data_processing/api_client.rs
+++ src/data_processing/api_client.rs
@@ -3,7 +3,7 @@ use governor::DefaultDirectRateLimiter;
 use reqwest;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use std::collections::{ HashMap, HashSet };
+use std::collections::{HashMap, HashSet};
 use std::error;
 use std::fmt;
 use std::marker::PhantomData;
@@ -16,7 +16,6 @@ const SUBJECT_URL: &str = "https://api.wanikani.com/v2/subjects";
 const ASSIGNMENT_URL: &str = "https://api.wanikani.com/v2/assignments";
 
 type ApiClientError = reqwest::Error;
-
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct User {
@@ -100,26 +99,41 @@ pub struct ReqwestResponse<T> {
 pub struct ApiClient<'a> {
     token: String,
     client: &'a reqwest::Client,
-    limiter: DefaultDirectRateLimiter
+    limiter: DefaultDirectRateLimiter,
 }
 
 impl<'a> ApiClient<'a> {
-    pub fn new(token: String, client: &'a reqwest::Client, limiter: DefaultDirectRateLimiter) -> Self {
-        ApiClient { token, client, limiter }
+    pub fn new(
+        token: String,
+        client: &'a reqwest::Client,
+        limiter: DefaultDirectRateLimiter,
+    ) -> Self {
+        ApiClient {
+            token,
+            client,
+            limiter,
+        }
     }
 
     async fn get_response<T>(&self, url: &str) -> Result<ReqwestResponse<T>, ApiClientError>
     where
         T: DeserializeOwned,
     {
-        Ok(self.get_response_with_params::<T, String>(url, None).await?)
+        Ok(self
+            .get_response_with_params::<T, String>(url, None)
+            .await?)
     }
 
-    async fn get_response_with_params<T, K>(&self, url: &str, params: Option<Vec<(&str, K)>>) -> Result<ReqwestResponse<T>, ApiClientError>
+    async fn get_response_with_params<T, K>(
+        &self,
+        url: &str,
+        params: Option<Vec<(&str, K)>>,
+    ) -> Result<ReqwestResponse<T>, ApiClientError>
     where
         T: DeserializeOwned,
-        K: Serialize {
-            let mut response = self
+        K: Serialize,
+    {
+        let mut response = self
             .client
             .get(url)
             .header("Authorization", format!("Bearer {}", self.token));
@@ -128,14 +142,13 @@ impl<'a> ApiClient<'a> {
             response = response.query(&valid_param)
         }
 
-
         let response = response.send().await?;
 
         Ok(ReqwestResponse::<T> {
             raw_response: response.error_for_status()?,
             resource_type: PhantomData,
         })
-        }
+    }
 
     async fn raw_response_to_data<T>(
         &self,
@@ -157,41 +170,53 @@ impl<'a> ApiClient<'a> {
     }
 
     pub async fn get_non_paginated_data<T>(&self, url: &str) -> Result<Response<T>, ApiClientError>
-        where T: DeserializeOwned {
-            self.limiter.until_ready().await;
-            let raw = self.get_response::<Response<T>>(url).await?;
-            let processed = self.raw_response_to_data(raw).await?;
+    where
+        T: DeserializeOwned,
+    {
+        self.limiter.until_ready().await;
+        let raw = self.get_response::<Response<T>>(url).await?;
+        let processed = self.raw_response_to_data(raw).await?;
 
-            Ok(processed)
-        }
+        Ok(processed)
+    }
 
     pub async fn get_all_pages_of_paged_data<T>(
         &self,
         paged_url: &str,
     ) -> Result<Vec<Response<T>>, ApiClientError>
-        where T: DeserializeOwned
+    where
+        T: DeserializeOwned,
     {
-        Ok(self.get_all_pages_of_paged_data_with_params::<T, String>(paged_url, None).await?)
+        Ok(self
+            .get_all_pages_of_paged_data_with_params::<T, String>(paged_url, None)
+            .await?)
     }
 
     pub async fn get_all_pages_of_paged_data_with_params<T, K>(
         &self,
         paged_url: &str,
-        params: Option<Vec<(&str, K)>>
+        params: Option<Vec<(&str, K)>>,
     ) -> Result<Vec<Response<T>>, ApiClientError>
-        where T: DeserializeOwned,
-        K: Serialize
+    where
+        T: DeserializeOwned,
+        K: Serialize,
     {
         self.limiter.until_ready().await;
 
         let raw = match params {
-            Some(_) => self.get_response_with_params::<PagedData<T>, K>(paged_url, params).await?,
-            None => self.get_response::<PagedData<T>>(paged_url).await?
+            Some(_) => {
+                self.get_response_with_params::<PagedData<T>, K>(paged_url, params)
+                    .await?
+            }
+            None => self.get_response::<PagedData<T>>(paged_url).await?,
         };
         let mut processed = self.raw_response_to_data(raw).await?;
         let mut result: Vec<Response<T>> = processed.data;
 
-        println!("Paged Data: {:?} Total Count: {:?}", processed.pages, processed.total_count);
+        println!(
+            "Paged Data: {:?} Total Count: {:?}",
+            processed.pages, processed.total_count
+        );
 
         while let Some(PageData {
             next_url: Some(ref url),
@@ -199,9 +224,7 @@ impl<'a> ApiClient<'a> {
         }) = processed.pages
         {
             self.limiter.until_ready().await;
-            let raw = self
-                .get_response::<PagedData<T>>(&url)
-                .await?;
+            let raw = self.get_response::<PagedData<T>>(&url).await?;
             processed = self.raw_response_to_data(raw).await?;
 
             result.append(&mut processed.data);
@@ -210,7 +233,10 @@ impl<'a> ApiClient<'a> {
         Ok(result)
     }
 
-    pub fn get_list_of_subjects_to_request(&self, review_stats: &Vec<Response<ReviewStatistic>>) -> Vec<i32> {
+    pub fn get_list_of_subjects_to_request(
+        &self,
+        review_stats: &Vec<Response<ReviewStatistic>>,
+    ) -> Vec<i32> {
         let mut result: HashSet<i32> = HashSet::new();
 
         println!("Length of review_stat vec: {:?}", review_stats.len());
@@ -222,11 +248,22 @@ impl<'a> ApiClient<'a> {
         result.into_iter().collect()
     }
 
-    pub async fn construct_id_to_subject_hash(&self, subject_list: &Vec<i32>) -> Result<HashMap<i32, Subject>, ApiClientError> {
-        let subject_list_strs: Vec<String> = subject_list.into_iter().map(|subject| subject.to_string()).collect();
+    pub async fn construct_id_to_subject_hash(
+        &self,
+        subject_list: &Vec<i32>,
+    ) -> Result<HashMap<i32, Subject>, ApiClientError> {
+        let subject_list_strs: Vec<String> = subject_list
+            .into_iter()
+            .map(|subject| subject.to_string())
+            .collect();
         let query_params = vec![("ids", subject_list_strs.join(","))];
-        let all_subjects: Vec<Response<Subject>> = self.get_all_pages_of_paged_data_with_params(SUBJECT_URL, Some(query_params)).await?;
-        let result: HashMap<i32, Subject> = all_subjects.into_iter().map( |response| (response.id.unwrap(), response.data)).collect();
+        let all_subjects: Vec<Response<Subject>> = self
+            .get_all_pages_of_paged_data_with_params(SUBJECT_URL, Some(query_params))
+            .await?;
+        let result: HashMap<i32, Subject> = all_subjects
+            .into_iter()
+            .map(|response| (response.id.unwrap(), response.data))
+            .collect();
 
         Ok(result)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use governor::{ RateLimiter, Quota };
+use governor::{Quota, RateLimiter};
 use nonzero_ext::*;
 use wanikani_stats::data_processing::api_client::{ApiClient, ReviewStatistic};
 
@@ -9,12 +9,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wk_token = std::env::var("WK_TOKEN").expect("WK_TOKEN must be set");
 
     let client = reqwest::Client::new();
-    let limiter = RateLimiter::direct_with_clock(Quota::per_minute(nonzero!(5u32)), governor::clock::DefaultClock::default());
+    let limiter = RateLimiter::direct_with_clock(
+        Quota::per_minute(nonzero!(5u32)),
+        governor::clock::DefaultClock::default(),
+    );
 
     let api_client = ApiClient::new(wk_token, &client, limiter);
 
     let user_data = api_client.get_user_data().await?;
-    let review_data = api_client.get_all_pages_of_paged_data::<ReviewStatistic>("https://api.wanikani.com/v2/review_statistics").await?;
+    let review_data = api_client
+        .get_all_pages_of_paged_data::<ReviewStatistic>(
+            "https://api.wanikani.com/v2/review_statistics",
+        )
+        .await?;
     let sub_vec = api_client.get_list_of_subjects_to_request(&review_data);
     let hashy = api_client.construct_id_to_subject_hash(&sub_vec).await?;
 
